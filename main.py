383f449f77c78d327409aa6dbd4d1b1f659cec42
JangEunjin2022042021 <112847633+JangEunjin2022042021@users.noreply.github.com>
2023-11-22 19:15:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -145,3 +145,55 @@ def redrawWindow(surface):
     snack.draw(surface)
     drawGrid(width, rows, surface)
     pygame.display.update()
+
+    def randomSnack(rows, item):
+
+        positions = item.body
+
+        while True:
+            x = random.randrange(rows)
+            y = random.randrange(rows)
+            if len(list(filter(lambda z: z.pos == (x, y), positions))) > 0:
+                continue
+            else:
+                break
+
+        return (x, y)
+
+    def message_box(subject, content):
+        root = tk.Tk()
+        root.attributte("-topmost", True)
+        root.withdraw()
+        messagebox.showinfo(subject, content)
+        try:
+            root.destroy()
+        except:
+            pass
+
+    def main():
+        global width, rows, s, snack
+        width = 500
+        rows = 20
+        win = pygame.display.set_mode((width, width))
+        s = snake((255, 0, 0), (10, 10))
+        snack = cube(randomSnack(rows, s), color=(0, 255, 0))
+        flag = True
+
+        clock = pygame.time.Clock()
+
+        while flag:
+            pygame.time.delay(50)
+            clock.tick(10)
+            s.move()
+            if s.body[0].pos == snack.pos:
+                s.addCube()
+                snack = cube(randomSnack(rows, s), color=(0, 255, 0))
+
+            for x in range(len(s.body)):
+                if s.body[x].pos in list(map(lambda z: z.pos, s.body[x + 1:])):
+                    print('Score: ', len(s.body))
+                    message_box('You Lost!', 'Play again...')
+                    s.reset((10, 10))
+                    break
+
+            redrawWindow(win)
